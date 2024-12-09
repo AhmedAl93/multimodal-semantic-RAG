c24ffb2435f851f3f0764e7e43c7734ba8240e27
Ahmed Allali <ahmed_allali@yahoo.com>
2024-12-09 02:18:24 +0100

    Fix logging handlers duplicates

--- src/tools.py
+++ src/tools.py
@@ -368,12 +368,13 @@ class Logger:
 
         # Set up the logger
         self.logger = logging.getLogger(logger_name)
-
-        fhandler = logging.FileHandler(filename=log_file, mode='w')
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        fhandler.setFormatter(formatter)
-        self.logger.addHandler(fhandler)
         self.logger.setLevel(log_level)
+
+        if not self.logger.handlers:
+            fhandler = logging.FileHandler(filename=log_file, mode='w')
+            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            fhandler.setFormatter(formatter)
+            self.logger.addHandler(fhandler)
 
     def get_logger(self):
         """
